240f2e537c6d9c1afccb7f85f9a61156c5249be7
HennadiyV <com.mastera@gmail.com>
2021-11-01 20:56:58 +0200

    Get information from the network

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, NIX Education")
+	client := http.Client{}
+	resp, err := client.Get("https://jsonplaceholder.typicode.com/posts")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%s \n", body)
 }
