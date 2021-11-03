4a6811268ac3956598f815ab5cdab359a99e1f54
HennadiyV <com.mastera@gmail.com>
2021-11-03 07:30:18 +0200

    Data get for each post in parallel

--- main.go
+++ main.go
@@ -4,19 +4,40 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+	"sync"
 )
 
 func main() {
-	client := http.Client{}
-	resp, err := client.Get("https://jsonplaceholder.typicode.com/posts")
+	url := "https://jsonplaceholder.typicode.com/posts"
+	var wg sync.WaitGroup
+	countPost := 100
+	wg.Add(countPost)
+	for i := 1; i <= countPost; i++ {
+		go getPost(&url, i, &wg)
+	}
+	wg.Wait()
+}
+
+func getPost(purl *string, postId int, pwg *sync.WaitGroup) {
+
+	defer pwg.Done()
+	url := *purl
+	url = url + "/" + strconv.Itoa(postId)
+
+	resp, err := http.Get(url)
+
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
+	sbt, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
-	fmt.Printf("%s \n", body)
+
+	fmt.Printf("%s  \n", sbt)
+
 }
