3f48a9358717763d75eb7434df4a4771ee66bc27
HennadiyV <com.mastera@gmail.com>
2021-11-05 21:22:12 +0200

    Posts save  in files

--- main.go
+++ main.go
@@ -1,25 +1,61 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 	"strconv"
+	"strings"
 	"sync"
 )
 
-func main() {
-	url := "https://jsonplaceholder.typicode.com/posts"
-	var wg sync.WaitGroup
-	countPost := 100
-	wg.Add(countPost)
-	for i := 1; i <= countPost; i++ {
-		go getPost(&url, i, &wg)
-	}
-	wg.Wait()
+type Post struct {
+	UserId int
+	Id     int
+	Title  string
+	Body   string
 }
 
-func getPost(purl *string, postId int, pwg *sync.WaitGroup) {
+func postCreateFromJson(b []byte) (p Post, err error) {
+
+	err = json.Unmarshal(b, &p)
+	if err != nil {
+
+		return p, err
+	}
+	return p, err
+
+}
+
+func main() {
+
+	url := "https://jsonplaceholder.typicode.com/posts"
+	dir := "storage/posts/"
+	pattern := "*.txt"
+	countPost := 100
+
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	err := os.MkdirAll(dir, 0777)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	var wg sync.WaitGroup
+	ch := make(chan []byte)
+
+	for i := 1; i <= countPost; i++ {
+		wg.Add(2)
+		go getPostToChanel(ch, &url, i, &wg, errorLog)
+		go savePostToFile(ch, dir+pattern, &wg, errorLog)
+	}
+	wg.Wait()
+
+}
+
+func getPostToChanel(c chan []byte, purl *string, postId int, pwg *sync.WaitGroup, errorLog *log.Logger) {
 
 	defer pwg.Done()
 	url := *purl
@@ -28,16 +64,37 @@ func getPost(purl *string, postId int, pwg *sync.WaitGroup) {
 	resp, err := http.Get(url)
 
 	if err != nil {
-		fmt.Println(err)
+		errorLog.Fatal(err)
 		return
 	}
 	defer resp.Body.Close()
-	sbt, err := io.ReadAll(resp.Body)
+
+	bresp, err := io.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println(err)
+		errorLog.Fatal(err)
 		return
 	}
 
-	fmt.Printf("%s  \n", sbt)
+	c <- bresp
+
+}
+func savePostToFile(c chan []byte, pattern string, pwg *sync.WaitGroup, errorLog *log.Logger) {
+
+	defer pwg.Done()
+
+	post, err := postCreateFromJson(<-c)
+	if err != nil {
+		errorLog.Print(err)
+		return
+	}
+	bpost := []byte(fmt.Sprint(post))
+	strid := strconv.Itoa(post.Id)
+	fileName := strings.Replace(pattern, "*", strid, 1)
+
+	err = ioutil.WriteFile(fileName, bpost, 0777)
+	if err != nil {
+		errorLog.Print(err)
+		return
+	}
 
 }
